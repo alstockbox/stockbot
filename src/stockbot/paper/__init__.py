0b165c6207dda4cc3515f68aeacd7f3ecbf25bc0
alstockbox <alwatchbox@gmail.com>
2026-09-08 22:27:02 +0200

    feat: add paper arena package

---
+++ src/stockbot/paper/__init__.py
@@ -0,0 +1,10 @@
+from stockbot.paper.arena import PaperArenaCriteria, PaperArenaReport, evaluate_paper_track
+from stockbot.paper.ledger import PaperObservation, PaperTradingLedger
+
+__all__ = [
+    "PaperArenaCriteria",
+    "PaperArenaReport",
+    "PaperObservation",
+    "PaperTradingLedger",
+    "evaluate_paper_track",
+]
