1d8f5367410660245ec3bc58c8f1e00a30b4e68d
alstockbox <alwatchbox@gmail.com>
2026-09-26 03:23:45 +0200

    chore: export shadow runtime primitives

--- src/stockbot/runtime/__init__.py
+++ src/stockbot/runtime/__init__.py
@@ -3,5 +3,24 @@ from stockbot.runtime.paper_runtime import (
     PaperTradePlan,
     PaperTradingRuntime,
 )
+from stockbot.runtime.shadow_readiness import ShadowReadinessReport, shadow_readiness
+from stockbot.runtime.shadow_runtime import (
+    ReadOnlyMarketProvider,
+    ShadowRuntimeDecision,
+    ShadowRuntimeResult,
+    ShadowTradePlan,
+    ShadowTradingRuntime,
+)
 
-__all__ = ["PaperRuntimeResult", "PaperTradePlan", "PaperTradingRuntime"]
+__all__ = [
+    "PaperRuntimeResult",
+    "PaperTradePlan",
+    "PaperTradingRuntime",
+    "ReadOnlyMarketProvider",
+    "ShadowReadinessReport",
+    "ShadowRuntimeDecision",
+    "ShadowRuntimeResult",
+    "ShadowTradePlan",
+    "ShadowTradingRuntime",
+    "shadow_readiness",
+]
